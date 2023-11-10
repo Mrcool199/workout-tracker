df12d1047e93359efe4374d72086e6d49065ab95
Mathew <mathewjohncorreia@gmail.com>
2023-11-10 14:00:19 +0000

    feat: made search bar work

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,14 +1,17 @@
 import { MenuButton } from "@/components/MenuButton";
 import { SearchBar } from "@/components/SearchBar";
 import { loginIsRequiredServer } from "@/lib/auth";
+import { getUsers } from "@/lib/api/workout/queries";
 
 export default async function group() {
   await loginIsRequiredServer();
 
+  const users = await getUsers();
+
   return (
     <div>
       <MenuButton user="" hidden="/" buttonName="Home" />
-      <SearchBar />
+      <SearchBar usersGot={users} />
     </div>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,46 @@
+"use client";
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { SwordsIcon } from "lucide-react";
-import { getUsers } from "@/lib/api/workout/queries";
 import Link from "next/link";
+import { useState } from "react";
 
-export async function SearchBar() {
-  const users = await getUsers();
+export function SearchBar({ usersGot }: { usersGot: any }) {
+  const [users] = useState(usersGot);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleInputChange(e: any) {
+    setSearchTerm(e.target.value);
+  }
+
+  function searching() {
+    if (!Array.isArray(users) || users.length === 0) {
+      return null;
+    }
+
+    const filteredUsers = users.filter((user) =>
+      user.name?.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return filteredUsers.map((user) => (
+      <Button
+        asChild
+        key={user.id}
+        variant="secondary"
+        className="justify-between border-b-2 my-2 w-full"
+      >
+        <Link href={`/users/${user.id}`} key={user.id}>
+          <div className="icon shadow w-8 h-8 p-0">
+            <img src={user?.image ?? ""} alt="" />
+          </div>
+          <span>{user?.name}</span>
+          <SwordsIcon />
+        </Link>
+      </Button>
+    ));
+  }
+
   return (
     <>
       <div className="px-4 flex flex-row items-baseline">
@@ -27,37 +62,15 @@ export async function SearchBar() {
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
-          <Input type="text" placeholder="Search" className="pl-12 pr-4" />
+          <Input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Search"
+            className="pl-12 pr-4"
+          />
         </div>
       </div>
-      <div className="flex flex-col p-4 ">
-        {Array.isArray(users) ? (
-          users.map((user: any) => (
-            <Button
-              asChild
-              key={user.id}
-              variant="secondary"
-              className="justify-between border-b-2 my-2 w-full"
-            >
-              <Link href={`/users/${user.id}`} key={user.id}>
-                <div className="icon shadow w-8 h-8 p-0">
-                  <img src={user?.image ?? ""} alt="" />
-                </div>
-                <span>{user?.name}</span>
-                <SwordsIcon />
-              </Link>
-            </Button>
-          ))
-        ) : (
-          <Button
-            variant="secondary"
-            className="justify-between border-b-2 my-2"
-          >
-            <span>No users found</span>
-            <SwordsIcon />
-          </Button>
-        )}
-      </div>
+      <div className="flex flex-col p-4 ">{searching()}</div>
     </>
   );
 }
