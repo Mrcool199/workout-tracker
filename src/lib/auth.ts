d6b88f4fde5731288f82eb3ac65c5da205f2a404
Mathew <mathewjohncorreia@gmail.com>
2023-11-13 08:42:23 +0000

    refactor: login again works

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,12 @@
-import nextAuth, {
-  DefaultSession,
-  NextAuthOptions,
-  User,
-  getServerSession,
-} from "next-auth";
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
-
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./db";
+import { and, eq } from "drizzle-orm";
+import { accounts, users } from "@/lib/db/schema/auth";
 
 declare module "next-auth" {
   interface Session {
@@ -21,7 +17,26 @@ declare module "next-auth" {
 }
 
 export const authConfig: NextAuthOptions = {
-  adapter: DrizzleAdapter(db),
+  debug: true,
+  adapter: {
+    // @ts-expect-error
+    ...DrizzleAdapter(db),
+    async getUserByAccount(providerAccountId) {
+      const results = await db
+        .select()
+        .from(accounts)
+        .leftJoin(users, eq(users.id, accounts.userId))
+        .where(
+          and(
+            eq(accounts.provider, providerAccountId.provider),
+            eq(accounts.providerAccountId, providerAccountId.providerAccountId)
+          )
+        )
+        .get();
+
+      return results?.user ?? null;
+    },
+  },
   callbacks: {
     session({ session, user }) {
       session.user.id = user.id;
